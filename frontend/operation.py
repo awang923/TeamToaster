a6033b404995cc2006487ac6ce4dc40b36904cd3
Annie Wang <awang144@ucr.edu>
2023-03-12 19:14:43 -0700

    add sign in

--- frontend/operation.py
+++ frontend/operation.py
@@ -16,6 +16,56 @@ class Operation(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
+        def sign_in_popup():
+            popup = Toplevel(self)
+            popup.geometry("700x250")
+            popup.title("Sign In")
+            #popup_label = Label(popup, text = "SIGN IN POPUP")
+            # popup_label.pack(pady=10)
+            username_input = Entry(popup, width=20)
+            username_input.bind(
+                '<Button-1>', lambda x: on_focus_in(username_input))
+            username_input.bind('<FocusOut>', lambda x: on_focus_out(
+                username_input, 'First Last'))
+            username_input.place(relx=.5, rely=.15, anchor=CENTER)
+            error_msg = Label(popup, text="")
+            error_msg.place(relx=.5, rely=.3, anchor=CENTER)
+
+            def open_error_popup():
+                error_msg.config(text="Enter your first name and last name")
+
+            def sign_in_on_click():
+                # print(username_input.get())
+                if(username_input.get() != "" and username_input.get() != "First Last"):
+                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    with open("TeamToaster/frontend/logfile.txt", 'a') as logfile:
+                        logfile.write(
+                            current_time + " " + username_input.get() + " signed in." + "\n")
+                    logfile.close()
+                    popup.destroy()
+                    # controller.show_frame(UploadManifestPage)
+                else:
+                    #print("ERROR MESSAGE")
+                    open_error_popup()
+            popup_sign_in_button = Button(
+                popup, text="Sign In", command=lambda: sign_in_on_click(), width=20)
+            popup_sign_in_button.place(relx=.5, rely=.5, anchor=CENTER)
+
+            def on_focus_in(entry):
+                if entry.cget('state') == 'disabled':
+                    entry.configure(state='normal')
+                    entry.delete(0, 'end')
+                    error_msg['text'] = ""
+
+            def on_focus_out(entry, placeholder):
+                if entry.get() == '':
+                    entry.insert(0, placeholder)
+                    entry.configure(state='disabled')
+
+        sign_in_button = Button(self, text="Sign In",
+                                command=lambda: sign_in_popup())
+        sign_in_button.place(relx=.8, rely=.1, anchor="e")
+
         #manifest = open(globals.string_filename, 'r')
         #manifest_lines = manifest.readlines()
         #regex = ".(\d\d),(\d\d).,\s{(\d*)}.\s([a-zA-Z]*)"
